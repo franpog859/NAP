d24937bf4181abeac7dd5db926ee418afd66bf4f
franpog859 <franpog859@student.polsl.pl>
2018-04-17 12:51:04 +0200

    alarm setting added

---
+++ app/src/main/java/com/example/fran/nap/AlarmReceiver.java
@@ -0,0 +1,16 @@
+package com.example.fran.nap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Fran on 17.04.2018.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/com/example/fran/nap/MainActivity.java
+++ app/src/main/java/com/example/fran/nap/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.fran.nap;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int timeInMillis = 1000 * 60 * 26;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setAlarm();
+    }
+
+    private void setAlarm() {
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getBaseContext(), 1, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, 1000 * 60 * 26,
+                pendingIntent);
     }
 }
