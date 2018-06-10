4621864a182ef9cdb25ca98c92c0e4b8470dec94
franpog859 <franpog859@student.polsl.pl>
2018-06-10 22:38:37 +0200

    wake up the screen

--- app/src/main/java/com/example/fran/nap/AlarmReceiver.java
+++ app/src/main/java/com/example/fran/nap/AlarmReceiver.java
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.PowerManager;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.widget.Toast;
 
+import static android.content.Context.POWER_SERVICE;
 import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
 
 /**
@@ -23,6 +25,13 @@ public class AlarmReceiver extends BroadcastReceiver {
 
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(400);
+
+        PowerManager powerManager = (PowerManager) context.getApplicationContext()
+                .getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
+                (PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP),
+                "TAG");
+        wakeLock.acquire();
 
         //Intent service = new Intent(context, AlarmIntentService.class);
         //startWakefulService(context, service);
