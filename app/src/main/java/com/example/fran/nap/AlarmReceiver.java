c2700690a755c313219f51547a4251957a0f2cc3
franpog859 <franpog859@student.polsl.pl>
2018-06-16 13:57:22 +0200

    added test NASA sound

--- app/src/main/java/com/example/fran/nap/AlarmReceiver.java
+++ app/src/main/java/com/example/fran/nap/AlarmReceiver.java
@@ -1,23 +1,8 @@
 package com.example.fran.nap;
 
-import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
-import android.os.CountDownTimer;
-import android.os.PowerManager;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.support.v4.content.WakefulBroadcastReceiver;
-import android.view.WindowManager;
-import android.widget.Toast;
-
-import java.util.Timer;
-
-import static android.content.Context.POWER_SERVICE;
-import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
 
 /**
  * Created by Fran on 17.04.2018.
@@ -46,8 +31,5 @@ public class AlarmReceiver extends BroadcastReceiver {
         KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         final KeyguardManager.KeyguardLock lock = keyguardManager.newKeyguardLock("TAG");
         lock.disableKeyguard();*/
-
-        //Intent service = new Intent(context, AlarmIntentService.class);
-        //startWakefulService(context, service);
     }
 }
