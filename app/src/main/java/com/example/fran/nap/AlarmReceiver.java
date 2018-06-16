cad314c64aaa7b493f77b7afbbab29a446285e51
franpog859 <franpog859@student.polsl.pl>
2018-06-16 14:20:07 +0200

    minor refactor

--- app/src/main/java/com/example/fran/nap/AlarmReceiver.java
+++ app/src/main/java/com/example/fran/nap/AlarmReceiver.java
@@ -12,24 +12,13 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        turnLockScreenActivityOn(context);
+    }
 
+    private void turnLockScreenActivityOn(Context context) {
         Intent lockScreen = new Intent(context, LockScreen.class);
         lockScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
                 Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(lockScreen);
-/*
-        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(400);
-
-        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
-                (PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP),
-                "TAG");
-        wakeLock.acquire();
-
-        //FIXIT: Something is wrong when screen wakes up and user taps the HOME button.
-        KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
-        final KeyguardManager.KeyguardLock lock = keyguardManager.newKeyguardLock("TAG");
-        lock.disableKeyguard();*/
     }
 }
--- app/src/main/java/com/example/fran/nap/MainActivity.java
+++ app/src/main/java/com/example/fran/nap/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.fran.nap;
 
-import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.os.PowerManager;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +12,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private final int timeInMillis = 1000 * 60 * 26;
+    private final int testTime = 1000 * 5; //TODO: Replace it later with timeInMillis.
 
     private AlarmManager alarmManager;
     private PendingIntent pendingIntent;
@@ -26,22 +22,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setAlarmReceiver();
         setAlarm();
     }
 
-    private void setAlarm() {
+    private void setAlarmReceiver() {
         Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(
                 getApplicationContext(), 1, intent, 0);
+    }
+    private void setAlarm() {
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime() + 1000 * 5, pendingIntent);
+                SystemClock.elapsedRealtime() + testTime, pendingIntent);
     }
 
-    public void cancelAlarm(View view) {
-        if (alarmManager!= null) {
+    public void cancelAlarmAndClose(View view) {
+        if (alarmManager != null) {
             alarmManager.cancel(pendingIntent);
-            //TODO: Close the app.
             closeApp();
         }
     }
