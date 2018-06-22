9694e64447ccea9c423ab1851343eb54d190f926
franpog859 <franpog859@student.polsl.pl>
2018-06-22 14:42:21 +0200

    added the Timer and sounds start one after the another

--- app/src/main/java/com/example/fran/nap/AudioService.java
+++ app/src/main/java/com/example/fran/nap/AudioService.java
@@ -3,6 +3,9 @@ package com.example.fran.nap;
 import android.content.Context;
 import android.media.MediaPlayer;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Fran on 21.06.2018.
  */
@@ -11,16 +14,36 @@ public class AudioService {
 
     private final static int INTRO_VOLUME = 50;
 
+    public static MediaPlayer mediaPlayer;
+
     public static void playIntro(Context context) {
         MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.intro);
         VolumeService.setMediaPlayerPercentVolume(mediaPlayer, INTRO_VOLUME);
         mediaPlayer.start();
     }
 
-    public static void startAlarmSounds(Context context) { //TODO
+    public static void startAlarmSounds(final Context context) { //TODO
+        setUpMediaPlayer(context);
+        setUpNextSound(context);
+    }
+    
+    private static void setUpMediaPlayer(Context context) {
         int soundId = SoundsRepository.getRandomSoundId();
-        MediaPlayer mediaPlayer = MediaPlayer.create(context, soundId);
-        VolumeService.setMediaPlayerPercentVolume(mediaPlayer, INTRO_VOLUME);
+        mediaPlayer = MediaPlayer.create(context, soundId);
+        VolumeService.setMediaPlayerPercentVolume(mediaPlayer, 100);
         mediaPlayer.start();
+    }
+
+    private static void setUpNextSound(final Context context) {
+        Timer timer = new Timer();
+        int soundDuration = mediaPlayer.getDuration();
+        TimerTask playNextSound = new TimerTask() {
+            @Override
+            public void run() {
+                mediaPlayer.reset();
+                AudioService.startAlarmSounds(context);
+            }
+        };
+        timer.schedule(playNextSound, soundDuration + 100);
     }
 }
